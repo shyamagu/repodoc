3f80def306bc4bb6efb30140669c6b07a5b20cef
Hiroaki Miyazaki <hbellstar@gmail.com>
2025-03-26 09:20:40 +0900

    Add API version configuration to README and update OpenAI client initialization

--- openai_utils.py
+++ openai_utils.py
@@ -9,7 +9,7 @@ load_dotenv()
 azure_openai_client = AzureOpenAI(
     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
     api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    api_version="2024-08-01-preview"
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION")
 )
 
 model_deployment_name = os.getenv("MODEL_DEPLOYMENT_NAME")
